25753efa4f2a699770b9b8e85ef9567671c37eba
Ayush-Numentica <ayush.ranjan@numentica-ui.com>
2025-08-05 15:37:06 +0530

    modified:   star_before_string.java

--- star_before_string.java
+++ star_before_string.java
@@ -1,14 +1,25 @@
 let str="code";
+let num=3;
 
-let count=str.length
-for(let i=1;i<=((count*2)+1);i++)
+// let a=str.padStart(7,"*")
+// let b=a.padEnd(10,"*")
+
+// console.log(b)
+let count = str.length-1
+function star(str, num)
 {
-    if(i>str.length && i<(str.length+2))
+    
+    let total_length= num+str.length+num
+    for(let i=0;i<total_length;i++)
     {
-        process.stdout.write(str)
-    }
-    else
-    {
-        process.stdout.write("*")
+        if(i>=num && i<(total_length-num))
+        {
+            process.stdout.write(str[i-num])
+        }
+        else
+        {
+            process.stdout.write("*")
+        }
     }
 }
+star(str, num)
