2b7a542b3329b114bffd7dc51b2091e76f137de0
Ayush-Numentica <ayush.ranjan@numentica-ui.com>
2025-08-08 16:53:59 +0530

    modified:   Day_4/sum-of-multiples.js

--- Day_4/sum-of-multiples.js
+++ Day_4/sum-of-multiples.js
@@ -1,3 +1,5 @@
+// Find sum of multiples
+
 function findingTheSumOfMultiple(inputNum, countOfMultiplication) {
     // checking whether is is number only
     if (typeof inputNum !== "number" || typeof countOfMultiplication !== "number") {
@@ -9,35 +11,36 @@ function findingTheSumOfMultiple(inputNum, countOfMultiplication) {
         return 0;
     }
 
-    // multiplying and finding the sum
-    let sumOfMultiple = 0;
-    let productExpression = "";
-    let productOfNum = 0;
-
     // when the count of multiplication is negative
     if (countOfMultiplication < 0) {
         countOfMultiplication = -countOfMultiplication;
     }
 
-    // for positive count of multiplication
+    // multiplying and finding the sum
+    let sumOfMultiple = 0;
+    let productExpression = "";
+
     for (let i = 1; i <= countOfMultiplication; i++) {
         productOfNum = inputNum * i;
         sumOfMultiple += productOfNum;
+
+        // for positive outpur of product with no bracket
         if (productOfNum > 0) {
-            productExpression += productOfNum;
+            productExpression += productOfNum +" ";
         }
 
         // specifying bracket before and after the negative number
         if (productOfNum < 0) {
             productExpression = productExpression + "(" + productOfNum + ")";
         }
+
         if (i !== countOfMultiplication) {
-            productExpression += "+"
+            productExpression += "+ "
         }
     }
-    return productExpression + "=" + sumOfMultiple;
+    return productExpression + "= " + sumOfMultiple;
 }
 
-let givenNum = 1;
-let numberOfMultiple = -5;
+let givenNum = 5;
+let numberOfMultiple = 5;
 console.log(findingTheSumOfMultiple(givenNum, numberOfMultiple));
