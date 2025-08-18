b9fe726ffb1f0dc51bc2377426f13e073f0084d8
Ayush-Numentica <ayush.ranjan@numentica-ui.com>
2025-08-18 17:08:03 +0530

    modified:   Day_9/Bill-generation.js 	new file:   node_modules/.package-lock.json 	new file:   node_modules/fuse.js/LICENSE 	new file:   node_modules/fuse.js/README.md 	new file:   node_modules/fuse.js/dist/fuse.basic.cjs 	new file:   node_modules/fuse.js/dist/fuse.basic.js 	new file:   node_modules/fuse.js/dist/fuse.basic.min.cjs 	new file:   node_modules/fuse.js/dist/fuse.basic.min.js 	new file:   node_modules/fuse.js/dist/fuse.basic.min.mjs 	new file:   node_modules/fuse.js/dist/fuse.basic.mjs 	new file:   node_modules/fuse.js/dist/fuse.cjs 	new file:   node_modules/fuse.js/dist/fuse.d.ts 	new file:   node_modules/fuse.js/dist/fuse.js 	new file:   node_modules/fuse.js/dist/fuse.min.cjs 	new file:   node_modules/fuse.js/dist/fuse.min.js 	new file:   node_modules/fuse.js/dist/fuse.min.mjs 	new file:   node_modules/fuse.js/dist/fuse.mjs 	new file:   node_modules/fuse.js/package.json 	new file:   package-lock.json 	new file:   package.json

--- Day_9/Bill-generation.js
+++ Day_9/Bill-generation.js
@@ -1,12 +1,97 @@
 // The data is the available inventory in the store. In the store you have various products with their price and available quantity.
 //Based on this data, write a function to generate the total bill of a grocery list given by an user
 
-function generateTheTotalBillFromUserList(givenGroceryList)
-{
+import Fuse from "fuse.js";
+
+function generateTheTotalBillFromUserList(givenInventoryData, givenGroceryList) {
     // checking whether the grocery list is an array
-    if(!Array.isArray(givenGroceryList) || givenGroceryList.length===0)
-    {
-        console.error("yu have enetered the empty list or the list is not in a proper format");
+    if (!Array.isArray(givenGroceryList) || !Array.isArray(givenInventoryData)) {
+        console.error("you have enetered the empty list or the list is not in a proper format");
         return false;
     }
+
+    // checking whether the list is data and list is empty
+
+    if (givenInventoryData.length === 0) {
+        console.error("your inventory is empty, add items in your inventory");
+        return false;
+    }
+    if (givenGroceryList.length === 0) {
+        console.error("your grocery list is empty, buy some items");
+        return false;
+    }
+
+
+
+    let totalBill = 0;
+    const fuse = new Fuse(givenInventoryData,
+        {
+            includeScore: true,
+            keys: ["item"],
+            threshold: 0.4
+        }
+    );
+
+    for (let i = 0; i < givenGroceryList.length; i++) {
+        // skipping the invalid inputs in grocery list list
+        if (typeof givenGroceryList[i] !== "object" || givenGroceryList[i].quantity < 0) {
+            continue;
+        }
+        // assuring that there must be three keys in inventory
+        if (Object.keys(givenGroceryList[i]).length < 2) {
+            console.error(`you have a key missing in your ${JSON.stringify(givenGroceryList[i])}`);
+            return false;
+        }
+        //checking for each inputs in the inventory list is in proper format or not
+        if (typeof givenGroceryList[i].item !== "string") {
+            console.error(`your entered item is incorrect in ${JSON.stringify(givenGroceryList[i])}`);
+            return false;
+        }
+        if (typeof givenGroceryList[i].quantity !== "number") {
+            console.error(`your entered quantity is incorrect in ${JSON.stringify(givenGroceryList[i])}`);
+            return false;
+        }
+
+
+        let requestedItem = givenGroceryList[i];
+        let itemInInventory = false;
+
+
+        const matcheditem = fuse.search(requestedItem.item);
+        // console.log(matcheditem);
+        if (matcheditem.length > 0) {
+            const availableItem = matcheditem[0].item;
+            itemInInventory = true;
+            let quantityOfItem = requestedItem.quantity;
+            if (quantityOfItem > availableItem.quantity) {
+                quantityOfItem = availableItem.quantity;
+                console.log(`Requested quantity for "${requestedItem.item}" exceeds inventory quantity. Using available quantity ${quantityOfItem}.`)
+            }
+            totalBill = totalBill + quantityOfItem * availableItem.price;
+            totalBill = Math.round(totalBill * 100) / 100;
+            availableItem.quantity = availableItem.quantity - quantityOfItem;
+        }
+        else {
+            console.warn(`the ${requestedItem.item} is not in the inventory`);
+        }
+    }
+    console.log(`the total bill of yours is ${totalBill}`);
+    return totalBill;
 }
+
+
+
+const inventoryItems = [
+    { item: "apple", price: 10.20, quantity: 10 },
+    { item: "banana", price: 20, quantity: 15 },
+    { item: "Jam - Apricot", price: 94.11, quantity: 2 },
+    { item: "Creamers - 10%", price: 15.20, quantity: 4 }
+];
+
+const groceryList = [
+    { item: "Jam - Apricot", quantity: 2 },
+    { item: "Creamers - 10%", quantity: 5 },
+    // { item: "Creamers - 10%", quantity:3},
+    // { item: "butter", quantity: 3 }
+];
+generateTheTotalBillFromUserList(inventoryItems, groceryList);
